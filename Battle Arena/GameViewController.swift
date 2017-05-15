010c665e1a75fe31b0709e6d2c492b810ab2dcb1
IslasGiga <islasgiga@gmail.com>
2017-05-15 10:53:12 -0300

    Create Load Battle Scene Method

--- Battle Arena/GameViewController.swift
+++ Battle Arena/GameViewController.swift
@@ -90,5 +90,32 @@ class GameViewController: UIViewController {
         }
     
     }
+
+     // MARK: Load Splash Screen Scene
+    
+    func loadBattleScene(){
+        if let scene = GKScene(fileNamed: "BattleScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! BattleScene? {
+                
+                // Copy gameplay related content over to the scene
+                
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .aspectFill
+                // Present the scene
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
+        
+    }
+
     
 }
