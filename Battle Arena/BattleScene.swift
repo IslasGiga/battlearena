5a9452406f55624d8a82f1f69e0dcacf61b8308d
Marcus Reuber Almeida Moraes Silva <marcusreuber@gmail.com>
2017-05-25 10:36:53 -0300

    Implementing deck cards rotation on batlescene (incomplete)

--- Battle Arena/BattleScene.swift
+++ Battle Arena/BattleScene.swift
@@ -35,6 +35,9 @@ class BattleScene: SKScene {
     //Cards on character menu
     var cards : [SKSpriteNode] = []
     
+    //Player Deck of Cards (as colors for testing)
+    var deck : [UIColor] = [UIColor.black, UIColor.blue, UIColor.orange, UIColor.red, UIColor.yellow, UIColor.green, UIColor.gray, UIColor.purple]
+    
     //current selected card
     var selectedCard = 5
     
@@ -59,7 +62,7 @@ class BattleScene: SKScene {
             
             if let battleNode = menuScene.childNode(withName: "BattleArea") as? SKSpriteNode {
                 self.battleNode = battleNode
-                //self.battleNode?.color = UIColor.clear
+                self.battleNode?.color = UIColor.clear
                 battleNode.removeFromParent()
                 self.addChild(battleNode)
                 
@@ -75,6 +78,9 @@ class BattleScene: SKScene {
         
         //loading Towers
         loadTowers()
+        
+        //loading Cards on menu
+        loadCards()
         
         
     }
@@ -228,7 +234,9 @@ class BattleScene: SKScene {
         }
     }
     
-    func loadSecundaryTowers(){
-        
+    func loadCards(){
+        for i in 0...4{
+            cards[i].color = deck[i]
+        }
     }
 }
