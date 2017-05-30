afb3d567624243ccb460cfc793c45de11c052d32
feliborgez <feliborgez@gmail.com>
2017-05-30 11:04:59 -0300

    edit(General): Finishing CardLoader ~ not tested ~

--- CardLoader.swift
+++ CardLoader.swift
@@ -10,5 +10,52 @@ import Foundation
 import SwiftyJSON
 
 class CardLoader {
- 
+    
+    func load(name: String) -> Card? {
+        let url = Bundle.main.url(forResource: name, withExtension: ".json")
+        
+        if let url = url {
+            
+            var data: Data?
+            
+            do {
+                data =
+                    try Data(contentsOf: url)
+                
+            } catch let error {
+                print("Got an error while loading the URL: \(error.localizedDescription)")
+            }
+            
+            if let data = data {
+                let json = JSON(data)
+                do {
+                    return try setCardAttributes(with: json)
+                } catch let error {
+                    print("Got an error while unpacking the JSON: \(error.localizedDescription)")
+                }
+            }
+            return nil
+            
+        } else {
+            return nil
+        }
+    }
+    
+    fileprivate func setCardAttributes(with json: JSON) throws -> Card {
+        
+        var card: Card
+        
+        guard let name = json["name"].string,
+              let description = json["description"].string,
+              let manaCost = json["manacost"].int,
+              let summoningTime = json["summoningtime"].int
+                else {
+               throw NSException(name: NSExceptionName(rawValue: "No Card Exception"), reason: "The card could not be initialized with the current info", userInfo: nil) as! Error
+        }
+        
+        card = Card(image: UIImage(named: "character")!, name: name, cardDescription: description, manaCost: manaCost, summoningTime: summoningTime, level: 1, xp: 1)
+        
+        return card
+    }
+    
 }
