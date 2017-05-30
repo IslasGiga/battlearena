4973670ac56114c8adcb2a2f16743fc137436e25
feliborgez <feliborgez@gmail.com>
2017-05-30 09:06:40 -0300

    Adding pods and CardLoader

---
+++ CardLoader.swift
@@ -0,0 +1,14 @@
+//
+//  CardLoader.swift
+//  Battle Arena
+//
+//  Created by Felipe Borges on 30/05/17.
+//  Copyright © 2017 Iracema Studio. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class CardLoader {
+ 
+}
