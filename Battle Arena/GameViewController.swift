58d551568d8291e7cec6b1f0186a2db245876f6a
IslasGiga <islasgiga@gmail.com>
2017-06-16 11:33:28 -0300

    Add user register view

--- Battle Arena/GameViewController.swift
+++ Battle Arena/GameViewController.swift
@@ -12,6 +12,18 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    
+    @IBAction func registerUser(_ sender: Any) {
+        
+        
+    }
+    
+    @IBOutlet weak var registerView: UIView!
+    
+    @IBOutlet weak var confirmButtom: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         
         let game = BattleScene()
@@ -26,10 +38,12 @@ class GameViewController: UIViewController {
             print(char.self)
         }
         
+        registerView.alpha = 0
+        
         
         super.viewDidLoad()
-        self.loadBattleScene()
-        
+        //self.loadBattleScene()
+        self.loadMainMenuScene()
         
     }
 
@@ -104,8 +118,36 @@ class GameViewController: UIViewController {
         }
     
     }
+    
+    // MARK: Load Splash Screen Scene
+    
+    func loadMainMenuScene(){
+        if let scene = GKScene(fileNamed: "MainMenuScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! MainMenuScene? {
+                
+                // Copy gameplay related content over to the scene
+                
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .aspectFill
+                sceneNode.gameViewController = self
+                // Present the scene
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
+        
+    }
 
-     // MARK: Load Splash Screen Scene
+
+     // MARK: Load Battle Scene
     
     func loadBattleScene(){
         if let scene = GKScene(fileNamed: "BattleScene") {
