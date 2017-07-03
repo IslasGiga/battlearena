6d4635e646a7570ca4b1d1f23529872d065fc913
Marcus Reuber Almeida Moraes Silva <marcusreuber@gmail.com>
2017-07-03 11:04:12 -0300

    Added new Turrent images to assets
    turrent images and size updates but Still need to scale with screen size

--- PrimaryTower.swift
+++ PrimaryTower.swift
@@ -12,14 +12,9 @@ class PrimaryTower: CharacterCard {
     
     
     override init(image: UIImage, name: String, cardDescription: String, manaCost: Int, summoningTime: Int, level: Int, xp: Int, atackPoints: Int, atackSpeed: CGFloat, atackArea: Int, atackRange: CGFloat, speed: Int, healthPoints: Int, battleScene: BattleScene, teamId: Int, cardImage: UIImage, atackEffect: String) {
-        super.init(image: image, name: name, cardDescription: cardDescription, manaCost: manaCost, summoningTime: 0, level: level, xp: xp, atackPoints: 25, atackSpeed: 0.5, atackArea: 1, atackRange: atackRange, speed: 0, healthPoints: 2000, battleScene: battleScene, teamId: teamId, cardImage: cardImage, atackEffect: "arrow")
+        super.init(image: image, name: name, cardDescription: cardDescription, manaCost: manaCost, summoningTime: 0, level: level, xp: xp, atackPoints: 70, atackSpeed: 0.5, atackArea: 1, atackRange: atackRange, speed: 0, healthPoints: 2000, battleScene: battleScene, teamId: teamId, cardImage: cardImage, atackEffect: "arrow")
         
-        self.spriteNode.size = CGSize(width: 40, height: 40)
-        if self.teamId == 0 {
-            self.spriteNode.color = UIColor.green
-        }else{
-            self.spriteNode.color = UIColor.orange
-        }
+        self.spriteNode.size = CGSize(width: 28, height: 40)
         
     }
     
--- SecundaryTower.swift
+++ SecundaryTower.swift
@@ -11,10 +11,10 @@ import GameplayKit
 class SecundaryTower: CharacterCard {
     
     override init(image: UIImage, name: String, cardDescription: String, manaCost: Int, summoningTime: Int, level: Int, xp: Int, atackPoints: Int, atackSpeed: CGFloat, atackArea: Int, atackRange: CGFloat, speed: Int, healthPoints: Int, battleScene: BattleScene, teamId: Int, cardImage: UIImage, atackEffect: String) {
-        super.init(image: image, name: name, cardDescription: cardDescription, manaCost: manaCost, summoningTime: 0, level: level, xp: xp, atackPoints: 20, atackSpeed: 0.5, atackArea: 1, atackRange: atackRange, speed: 0, healthPoints: 1000, battleScene: battleScene, teamId: teamId, cardImage: cardImage, atackEffect: "arrow")
+        super.init(image: image, name: name, cardDescription: cardDescription, manaCost: manaCost, summoningTime: 0, level: level, xp: xp, atackPoints: 50, atackSpeed: 0.5, atackArea: 1, atackRange: atackRange, speed: 0, healthPoints: 1000, battleScene: battleScene, teamId: teamId, cardImage: cardImage, atackEffect: "arrow")
         
         
-        self.spriteNode.size = CGSize(width: 32, height: 32)
+        self.spriteNode.size = CGSize(width: 27, height: 30)
         
     }
     
