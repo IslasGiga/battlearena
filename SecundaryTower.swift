cd93cb54dbcea8fa9cc29f79b9cd1ccae23d0e19
Marcus Reuber Almeida Moraes Silva <marcusreuber@gmail.com>
2017-07-05 18:47:21 -0300

    Added new particles for turrent atacks
    Turrent atack animation modified

--- SecundaryTower.swift
+++ SecundaryTower.swift
@@ -12,7 +12,7 @@ class SecundaryTower: CharacterCard {
     
 
         override init(image: UIImage, name: String, cardDescription: String, manaCost: Int, summoningTime: Int, level: Int, xp: Int, atackPoints: Int, atackSpeed: CGFloat, atackArea: Int, atackRange: CGFloat, speed: Int, healthPoints: Int, battleScene: BattleScene, teamId: Int, cardImage: UIImage, atackEffect: String, nodeSize: CGSize, soundName: String) {
-        super.init(image: image, name: name, cardDescription: cardDescription, manaCost: manaCost, summoningTime: 0, level: level, xp: xp, atackPoints: 50, atackSpeed: 1, atackArea: 1, atackRange: atackRange - 20, speed: 0, healthPoints: 1000, battleScene: battleScene, teamId: teamId, cardImage: cardImage, atackEffect: "arrow", nodeSize: CGSize(width: 27, height: 30), soundName: soundName)
+        super.init(image: image, name: name, cardDescription: cardDescription, manaCost: manaCost, summoningTime: 0, level: level, xp: xp, atackPoints: 50, atackSpeed: 1, atackArea: 1, atackRange: atackRange - 20, speed: 0, healthPoints: 1000, battleScene: battleScene, teamId: teamId, cardImage: cardImage, atackEffect: atackEffect, nodeSize: CGSize(width: 27, height: 30), soundName: soundName)
             self.spriteNode.physicsBody?.pinned = true
 
     }
